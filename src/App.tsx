0343ecd1cc7f66ba1ebee74ad2208919452020ca
jeantivan <jptivan53@gmail.com>
2023-07-05 21:48:19 -0400

    ADD: Editor de Lexical y lista de mensajes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import { Button } from "@/components/ui/button";
+import { RichText } from "@/components/rich-text";
+import LexicalRoot from "@/components/lexical-root";
+import { MessageItem } from "@/components/ui/message-item";
+
+import useMessageStore from "@/lib/store";
+import { TMessage } from "@/lib/types";
+
+const mapElementsToRender = (elements: Map<string, TMessage>) => {
+  const list = [];
+
+  for (const [id, value] of elements) {
+    list.push(<MessageItem key={id} {...value} />);
+  }
+
+  return list;
+};
 
 function App() {
-  return <div className="p-4"></div>;
+  const messages = useMessageStore((state) => state.messages);
+
+  const renderMessages = mapElementsToRender(messages);
+
+  return (
+    <div className="h-screen">
+      <div className="p-4 w-full mx-auto max-w-[800px]">
+        <div className="bg-white p-1.5 w-full rounded-md mb-4">
+          <LexicalRoot>
+            <RichText />
+          </LexicalRoot>
+        </div>
+        <div>
+          {messages.size <= 0 ? (
+            <div className="text-2xl">No messages to show.</div>
+          ) : (
+            renderMessages
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
