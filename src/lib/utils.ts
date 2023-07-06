75ec78eaae36c0647624b26e70c12ba22669f938
jeantivan <jptivan53@gmail.com>
2023-07-05 21:42:27 -0400

    ADD: Mas utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { v4 as uuid } from "uuid";
+
+import useMessageStore from "@/lib/store";
+
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+
+import { TMessage } from "@/lib/types";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateMessage(content: string): TMessage {
+  return {
+    id: uuid(),
+    isFav: false,
+    isOwn: true,
+    status: "received",
+    createdAt: dayjs().format(),
+    message: {
+      content,
+    },
+  };
+}
+
+export function createMessage(content: string) {
+  const newMessage = generateMessage(content);
+
+  useMessageStore.setState((prev) => ({
+    messages: new Map(prev.messages).set(newMessage.id, newMessage),
+  }));
 }
