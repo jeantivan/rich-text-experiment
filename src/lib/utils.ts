36201f651d25ad69283b616230df7020b331db98
jeantivan <jptivan53@gmail.com>
2023-08-27 16:51:38 -0400

    Util generateMessage cambiada

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,6 @@ import { twMerge } from "tailwind-merge";
 
 import { v4 as uuid } from "uuid";
 
-import useMessageStore from "@/lib/store";
-
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
@@ -15,23 +13,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function generateMessage(content: string): TMessage {
+export function generateMessage(body: string): TMessage {
   return {
     id: uuid(),
     isFav: false,
     isOwn: true,
     status: "received",
     createdAt: dayjs().format(),
-    message: {
-      content,
-    },
+    body
   };
-}
-
-export function createMessage(content: string) {
-  const newMessage = generateMessage(content);
-
-  useMessageStore.setState((prev) => ({
-    messages: new Map(prev.messages).set(newMessage.id, newMessage),
-  }));
 }
