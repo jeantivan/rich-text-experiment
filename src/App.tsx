70c310deef619fb26f2417d9ab3b1ec80765f625
jeantivan <jptivan53@gmail.com>
2023-08-27 17:08:35 -0400

    Cambiada un poco la estructura de la App

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
-import { RichText } from "@/components/rich-text";
-import LexicalRoot from "@/components/lexical-root";
-import { MessageItem } from "@/components/ui/message-item";
+import { MessageItem } from "@/components/message-item";
+import { CreateMessage } from "@/components/create-message";
 
-import useMessageStore from "@/lib/store";
-import { TMessage } from "@/lib/types";
-
-const mapElementsToRender = (elements: Map<string, TMessage>) => {
-  const list = [];
-
-  for (const [id, value] of elements) {
-    list.push(<MessageItem key={id} {...value} />);
-  }
-
-  return list;
-};
+import { useMessageStore } from "@/lib/store";
 
 function App() {
   const messages = useMessageStore((state) => state.messages);
 
-  const renderMessages = mapElementsToRender(messages);
-
   return (
-    <div className="h-screen">
-      <div className="p-4 w-full mx-auto max-w-[800px]">
-        <div className="bg-white p-1.5 w-full rounded-md mb-4">
-          <LexicalRoot>
-            <RichText />
-          </LexicalRoot>
-        </div>
+    <div className="h-screen ">
+      <div className="p-4 w-full max-w-[1000px] mx-auto">
+        <CreateMessage />
         <div>
-          {messages.size <= 0 ? (
-            <div className="text-2xl">No messages to show.</div>
+          {messages.length <= 0 ? (
+            <div className="text-center my-8 text-2xl">
+              No messages to show.
+            </div>
           ) : (
-            renderMessages
+            <div className="my-4">
+              {messages.map((message) => {
+                return <MessageItem key={message.id} {...message} />;
+              })}
+            </div>
           )}
         </div>
       </div>
